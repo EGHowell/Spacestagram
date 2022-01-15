35fd316df831f19b37ec24dba3870e3ac532a337
Eric Howell <e_howell@live.com>
2022-01-15 00:18:12 -0500

    implemented css design, structure; added heart like animation, individual post transform hover effects, reordered array to display most recent APOD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+
 import axios from 'axios';
 import { useEffect, useState } from 'react'
 import LikeButton from './LikeButton'
 
+
+
+
+
+
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [nasaData, setNasaData] = useState([]);
-  const [likedPhoto, setLikedPhoto] = useState(false)
 
   useEffect(() => {
     axios({
@@ -15,32 +20,62 @@ function App() {
       dataResponse: 'json',
       params: {
         api_key: 'CT3naHr22Inc0Dye93NCJK9GIAXoaHdxl2i0jUM7',
-        start_date: '2021-12-25'
+        start_date: '2022-01-01'
       }
     }).then((response) => {
-      setIsLoading(false)
-      setNasaData(response.data)
+      setIsLoading(false);
+      setNasaData(response.data.reverse());
+      
+      
     })
   }, [])
 
+  const newest = () => {
+    return nasaData.reverse()
+  }
 
 
   return (
     <div className="App">
-      {
-        isLoading ? <div className = "loading">Loading&#8230;</div> :
-        nasaData.map(photo => {
-          return ( 
-          <li key = {photo.title}>
-            <h2>{photo.title}</h2>
-            <p>{photo.date} </p>
-            <img src={photo.hdurl}/> 
-            <div id='root'>
-              <LikeButton/>
-            </div>
-          </li> )
-        })
-      }
+
+      <body className='wrapper'>
+
+        <header>
+          <h1>SPACESTAGRAM</h1>
+          <p>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
+        </header>
+        
+        <main>
+          {
+            isLoading ? <div className = "loading">Loading&#8230;</div> :
+            nasaData.reverse().map(function(photo, index) {
+         
+              return ( 
+                
+                  <li className="post" key = {index}>
+                    <div key = {photo.title} id = {photo.title}>
+                      <h2>{photo.title}</h2>
+                      <p>{photo.date} </p>
+                      <img src={photo.hdurl}/>
+                      <div className='postData'>
+                        <LikeButton />
+                        <p className='postInfo'>{photo.explanation}</p>
+                      </div>
+                    </div>
+                  </li> 
+                   
+              )
+            })
+          }
+        </main>
+        
+        <footer>
+          <p>
+            &copy; Eric Howell 2022
+          </p>
+        </footer>
+      </body>
+      
     </div>
   );
 }
--- src/LikeButton.js
+++ src/LikeButton.js
@@ -1,16 +1,28 @@
-import { useState } from 'react'
+import { useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons'
+
 
 function LikeButton() {
-    const [likedPhoto, setLikedPhoto] = useState(false)
+    const [userLike, setUserLike] = useState(false)
+
+    const dislikeHeartButton = <FontAwesomeIcon icon={faHeart} />
+    const likeHeartButton = <FontAwesomeIcon icon={faHeartSolid} />
+
+
 
     const toggleClass = () => {
-        setLikedPhoto(!likedPhoto);
+        setUserLike(!userLike);
     };
 
+
     return (
-        <button className={likedPhoto ? "favorite" : null} onClick = {toggleClass}>
-            {likedPhoto ? "yes" : "no"}
-        </button>
+            
+            <button onClick={toggleClass}>
+                {userLike ? likeHeartButton : dislikeHeartButton }
+            </button>   
+       
     )
 }
 
